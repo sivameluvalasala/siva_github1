2de2011b473fedf3bf90af7b67890e3d94d7a9bc
HP <HP@DESKTOP-7E6CDAO>
2023-05-30 13:36:40 +0530

    Build B02

--- TestNg_framework/src/Data_driven_testing/Set_data.java
+++ TestNg_framework/src/Data_driven_testing/Set_data.java
@@ -22,5 +22,6 @@ public static void main(String[] args) throws EncryptedDocumentException, IOExce
 	FileOutputStream fos=new FileOutputStream("./excel/data.xlsx");
 	book.write(fos);
 	//karthik thalamamsaa
+	//siva
 }
 }
